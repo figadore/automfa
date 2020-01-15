74df2a0ceee51948232b0e2a1c3086e4e3a41f41
Reese Wilson <3253971+figadore@users.noreply.github.com>
2020-01-15 11:19:56 -0800

    add the ability to add mfa secrets

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Usage
-Add a key to the keyring in the format `automfa_<service>` where the "user" is `automfa`. Then run `automfa <service>` to get a TOTP
+
+Add a key to the keyring with `automfa -a [-c] <service>`
+
+Run `automfa <service>` to get a TOTP
--- mfa.go
+++ mfa.go
@@ -4,21 +4,29 @@ import (
 	"crypto/sha256"
 	"flag"
 	"fmt"
+	"golang.org/x/crypto/ssh/terminal"
 	"log"
+	"os"
 
 	"github.com/creachadair/otp"
 	"github.com/zalando/go-keyring"
 )
 
 type options struct {
-	account string
-	service string
+	add       bool
+	account   string
+	service   string
+	clearText bool
 }
 
 func main() {
 	opts, err := parseArgs()
 	if err != nil {
 		log.Fatalf("Error parsing args: %v", err)
+	}
+	if opts.add {
+		addService(opts)
+		return
 	}
 	cfg := otp.Config{
 		Hash:   sha256.New,
@@ -35,6 +43,31 @@ func main() {
 	fmt.Println(cfg.TOTP())
 }
 
+func addService(opts *options) {
+	var secret string
+	var e error
+	fmt.Printf("Enter shared-secret (aka MFA seed) for %s:", opts.service)
+	if opts.clearText {
+		_, e = fmt.Scanln(&secret)
+	} else {
+		var secretBytes []byte
+		secretBytes, e = terminal.ReadPassword(int(os.Stdin.Fd()))
+		secret = string(secretBytes)
+		// Start new line after prompt
+		fmt.Println("")
+	}
+
+	if e != nil {
+		log.Fatalln("Error reading secret from stdin:", e)
+	}
+	keyring_key := fmt.Sprintf("automfa_%s", opts.service)
+	e = keyring.Set(keyring_key, opts.account, secret)
+	if e != nil {
+		// Secret is in memory, but unable to write it to the OS keyring. Continue with warning
+		log.Println("Error writing secret:", e)
+	}
+}
+
 // Handle any complicated arg parsing here
 func parseArgs() (*options, error) {
 	// go-keyring module uses two keys to look up a secret, 'account' and
@@ -43,10 +76,12 @@ func parseArgs() (*options, error) {
 	// username and password as two separate secrets
 	opts := options{}
 	opts.account = "automfa"
+	flag.BoolVar(&opts.add, "a", false, "Add a new MFA secret")
+	flag.BoolVar(&opts.clearText, "c", false, "If set, show text while typing (useful where copy/paste unavailable)")
 	flag.Parse()
 	args := flag.Args()
 	if len(args) < 1 {
-		return &options{}, fmt.Errorf("expecting at least 1 arg (secret_name), found %d", len(args))
+		return &options{}, fmt.Errorf("expecting at least 1 arg (service), found %d", len(args))
 	}
 	opts.service = args[0]
 	return &opts, nil
